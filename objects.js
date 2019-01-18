86034f72db760863f9541e35908ff74f79914a20
Matthew Luvin <jmluvin@gmail.com>
2019-01-18 17:56:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,6 +3,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle })
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName
-  return playlist
+  delete playlist.Slowdive
 }
