f7643d60a7fefe695efdf7835e7a29d6ef23784d
Matthew Luvin <jmluvin@gmail.com>
2019-01-18 17:43:53 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { "My Bloody Valentine": 'goodbye angels', 'Phil Ochs':, 'Slowdive': }
+var playlist = { "My Bloody Valentine": 'goodbye angels', 'Phil Ochs': 'hey', 'Slowdive': 'hiya' }
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({}, playlist, { [artistName]: songTitle })
 }
